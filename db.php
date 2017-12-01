9b8fa9af658e268be5da4c94246325fef698cb65
jdelongs <200339389@student.georgianc.on.ca>
2017-12-01 05:02:07 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
 //connect to the database
-$conn = new PDO('mysql:host=sql.computerstudi.es;dbname=gc200339389', 'gc200339389', 'dFqU^s25');
+$conn = new PDO('mysql:host=host;dbname=dbname', 'user', 'password');
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
